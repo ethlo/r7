109f67c545d6f350a438cb4751dfb4dd0efadf91
Morten Haraldsen <dev@ethlo.com>
2026-02-13 18:30:30 +0100

    Remove unused

--- venturi-core/src/main/java/com/ethlo/venturi/constants/VenturiConstants.java
+++
@@ -1,6 +0,0 @@
-package com.ethlo.venturi.constants;
-
-public final class VenturiConstants {
-    public static final CharSequence AUDIT_LEVEL_REQUEST     = "venturi.audit.request.level";
-    public static final CharSequence AUDIT_LEVEL_RESPONSE = "venturi.audit.response.level";
-}
