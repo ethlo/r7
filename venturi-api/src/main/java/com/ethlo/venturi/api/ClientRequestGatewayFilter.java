35368350e2aff7df7fb752ef9b526d714aa0868f
Morten Haraldsen <dev@ethlo.com>
2026-03-09 09:41:02 +0100

    Javadoc

--- venturi-api/src/main/java/com/ethlo/venturi/api/ClientRequestGatewayFilter.java
+++ venturi-api/src/main/java/com/ethlo/venturi/api/ClientRequestGatewayFilter.java
@@ -1,20 +1,20 @@
 package com.ethlo.venturi.api;
 
-public interface ClientRequestGatewayFilter
-{
+public interface ClientRequestGatewayFilter {
 
-    void onClientRequest(ClientRequestGatewayExchange exchange);
+    void onClientRequest(final ClientRequestGatewayExchange exchange);
 
     /**
      * Indicates whether this filter performs blocking operations, such as
-     * synchronous database queries or external HTTP calls.
-     * * By default, filters execute inline on the highly optimized Undertow IO thread.
-     * If this method returns true, the gateway engine will dispatch the execution
-     * of this filter (and subsequent request filters) to a Virtual Thread. This
-     * prevents the blocking operation from stalling the underlying non-blocking
-     * network event loop.
+     * synchronous database queries, heavy cryptographic work, or external HTTP calls.
+     * <p>
+     * By default, filters are expected to be non-blocking and execute inline on the
+     * server's primary network I/O thread to maximize throughput. If this method
+     * returns true, the underlying gateway engine is instructed to dispatch the
+     * execution of this filter to a dedicated worker thread (e.g., a Virtual Thread).
+     * This ensures that blocking operations do not stall the server's network event loop.
      *
-     * @return true if the filter requires dispatching off the IO thread, false otherwise.
+     * @return true if the filter requires dispatching off the primary I/O thread, false otherwise.
      */
     default boolean requiresDispatch()
     {
